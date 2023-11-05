ec906e9ecdb820b952e797c43ee9551b0a2ddd1c
Thtnaw Bezuwork <thtnawbuze@gmail.com>
2023-11-05 16:25:00 -0500

    another end point added

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.get("/", (req, res) => {
   res.send("Halo there! yes it works dsds");
 });
 
+app.get("/api", (req, res) => {
+  res.send("another api endpoint");
+});
 // app.listen(5000, (req, res) => {
 //   console.log("listening on port 5000...");
 // });
